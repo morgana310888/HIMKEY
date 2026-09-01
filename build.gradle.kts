891281faabef856f62a441b57f688a8ef490b668
Deizi <deizi.morgana@gmail.com>
2026-09-01 12:54:07 -0300

    primeira versao funcional do HIM

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("com.android.application") version "8.12.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
 
 allprojects {
